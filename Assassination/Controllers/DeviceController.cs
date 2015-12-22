cf6f55c6abd6d671f47778e4c1dc8554c961135d
dylanthelion <dillion256@gmail.com>
2015-12-21 18:02:34 -0600

    SAINT'S ROW, BITCHES!

---
+++ Assassination/Controllers/DeviceController.cs
@@ -0,0 +1,105 @@
+﻿using Assassination.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace Assassination.Controllers
+{
+    public class DeviceController : ApiController
+    {
+        AssassinationContext db = new AssassinationContext();
+
+        [HttpPost]
+        public HttpResponseMessage AddDeviceToAccount(string userName, string password, string UUID)
+        {
+            Player checkPlayer = (from check in db.AllPlayers
+                                  where check.UserName == userName
+                                  select check).FirstOrDefault();
+            Device checkDevice = (from check in db.AllDevices
+                                  where check.UUID == UUID
+                                  select check).FirstOrDefault();
+
+            if (checkPlayer == null)
+            {
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent(JArray.FromObject(new List<String>() { "Invalid user name" }).ToString(), Encoding.UTF8, "application/json")
+                };
+            }
+
+            if (checkPlayer.Password != password)
+            {
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent(JArray.FromObject(new List<String>() { "Invalid password" }).ToString(), Encoding.UTF8, "application/json")
+                };
+            }
+
+            if (checkDevice != null)
+            {
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent(JArray.FromObject(new List<String>() { "Device is already attached to an account" }).ToString(), Encoding.UTF8, "application/json")
+                };
+            }
+
+            Device d = new Device(checkPlayer, UUID);
+            db.AllDevices.Add(d);
+            db.SaveChanges();
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(JArray.FromObject(new List<String>() { String.Format("User ID: {0}", checkPlayer.ID.ToString()) }).ToString(), Encoding.UTF8, "application/json")
+            };
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage RemoveDevice(int playerID, string password, string UUID)
+        {
+            Player checkPlayer = db.AllPlayers.Find(playerID);
+
+            Device checkDevice = (from check in db.AllDevices
+                                  where check.UUID == UUID
+                                  select check).FirstOrDefault();
+
+            if (checkPlayer == null)
+            {
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent(JArray.FromObject(new List<String>() { "Invalid user ID" }).ToString(), Encoding.UTF8, "application/json")
+                };
+            }
+
+            if (checkPlayer.Password != password)
+            {
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent(JArray.FromObject(new List<String>() { "Invalid password" }).ToString(), Encoding.UTF8, "application/json")
+                };
+            }
+
+            if (checkDevice == null)
+            {
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent(JArray.FromObject(new List<String>() { "Invalid device ID" }).ToString(), Encoding.UTF8, "application/json")
+                };
+            }
+
+            db.AllDevices.Remove(checkDevice);
+            db.Entry(checkDevice).State = EntityState.Deleted;
+            db.SaveChanges();
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(JArray.FromObject(new List<String>() { "Deleted!" }).ToString(), Encoding.UTF8, "application/json")
+            };
+        }
+    }
+}
